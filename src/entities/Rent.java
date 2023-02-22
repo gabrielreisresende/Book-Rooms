5b295e20d1bc429e81be87e76012439afcfecb63
Gabriel Resende <gabriellrr2005@gmail.com>
2023-02-22 14:38:00 -0300

    Project created

---
+++ src/entities/Rent.java
@@ -0,0 +1,17 @@
+package entities;
+
+public class Rent {
+
+	private String name;
+	private String email;
+
+	public Rent(String name, String email) {
+		this.name = name;
+		this.email = email;
+	}
+
+	public String toString() {
+		return name + ", " + email;
+	}
+
+}
